056c100fe4f148ff8db85673bd6c4a6c14804d08
yun <luo00026@algonquinlive.com>
2016-11-17 14:20:17 -0500

    first version

--- app/src/main/java/com/sun/abby/cst2335_finalproject/HouseSettingFragment.java
+++ app/src/main/java/com/sun/abby/cst2335_finalproject/HouseSettingFragment.java
@@ -5,17 +5,52 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * Created by Abby on 2016-11-06.
- */
 
 public class HouseSettingFragment extends Fragment {
-
+    private ListView listView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.house_setting, container, false);
-        return rootView;
+
+
+
+            // Get ListView object
+            listView = (ListView) rootView.findViewById(R.id.home_List);
+
+            //Define Array value to show in ListView
+            String[] listValues= new String[]{"Garage","House Temperature","Outdoor Weather"};
+
+            // Define a new Adapter
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                    (getContext(),android.R.layout.simple_list_item_1,listValues);
+
+            listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //item index
+                int itemPosition = position;
+
+                //Item value
+                String itemValue = (String)listView.getItemAtPosition(position);
+
+                //Alert
+                Toast.makeText(getContext(),"Position: "+ itemPosition+" List Item: "
+                        +itemValue, Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+
+
+
+            return rootView;
     }
-}
+}// end of HouseSettingFragment
--- app/src/main/java/com/sun/abby/cst2335_finalproject/LivingRoomFragment.java
+++ app/src/main/java/com/sun/abby/cst2335_finalproject/LivingRoomFragment.java
@@ -5,17 +5,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by Abby on 2016-11-06.
  */
 
 public class LivingRoomFragment extends Fragment {
+    ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.living_room, container, false);
+
+        View rootView = inflater.inflate(R.layout.house_setting, container, false);
+
+
+
         return rootView;
     }
 }
